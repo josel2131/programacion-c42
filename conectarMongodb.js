af18d29285b2c8e90bb3f68ca9f9e9efa1d0f68f
josel2131 <113181622+josel2131@users.noreply.github.com>
2022-12-03 21:01:38 -0500

    Update conectarMongodb.js

--- conectarMongodb.js
+++ conectarMongodb.js
@@ -7,20 +7,6 @@ export const URI = mongoose.connect(process.env.MON_DBURI).then(()=>{
 }).catch((error) => console.error("Problemas al conectar a la base de datos de mongodb"));
 
 export default URI;
-/*
-export const URI = 'mongodb+srv://chunpun:Mimama1985+@cluster0.jrylw3n.mongodb.net/dbEventos';
-//vs'mongodb+srv://chunpun:Mimama1985+@cluster0.jrylw3n.mongodb.net/test'
-
-//shell 'mongodb+srv://chunpun:Mimama1985+@cluster0.jrylw3n.mongodb.net/dbEventos?retryWrites=true&w=majority';
-
-try {
-    const db = await mongoose.connect(URI);
-    console.log("estas conectao a:", db.connection.name);
-   } catch (error) {
-    console.error(error);
-   }
-
-   export default URI ;*/
 
 
   
